34a263bb7f65b273fd2e83b4a8e34a3f8dd6e967
Maxime Devalland <mdevalland@opentable.com>
2018-07-10 16:19:32 +0100

    fix run.sh

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,12 @@
 class singularity (
   String            $carbon_otenv         = $::singularity::params::carbon_otenv,
   String            $external_host        = $::singularity::params::external_host,
+  String            $from_email           = $::singularity::params::from_email,
   String            $install_dir          = $::singularity::params::install_dir,
   String            $install_method       = $::singularity::params::install_method,
   String            $group                = $::singularity::params::group,
   Array[String]     $master_list          = $::singularity::params::master_list,
-  Integer           $maxbuffers           = $::singularity::params::maxbuffers,
+  String            $notify_email         = $::singularity::params::notify_email,
   String            $service_name         = $::singularity::params::service_name,
   String            $service_provider     = $::singularity::params::service_provider,
   String            $singularity_url      = $::singularity::params::singularity_url,
@@ -14,8 +15,7 @@ class singularity (
   Optional[String]  $systemd_unit_want    = $::singularity::params::systemd_unit_want,
   String            $systemd_unit_after   = $::singularity::params::systemd_unit_after,
   String            $user                 = $::singularity::params::user,
-  String            $from_email           = $::singularity::params::from_email,
-  String            $notify_email         = $::singularity::params::notify_email,
+  String            $zookeeper_max_buffer = $::singularity::params::zookeeper_max_buffer,
 ) inherits singularity::params {
 
   include singularity::install
