8575a84ea48fc246fd23f035c73bd20157d0138b
Vishal Uderani <vuderani@opentable.com>
2019-02-04 23:08:28 +0530

    Fix typo in variable name

--- manifests/params.pp
+++ manifests/params.pp
@@ -22,7 +22,7 @@ class singularity::params {
   $zookeeper_max_buffer = 16777200,
   $mesos_psql_username  = hiera('mesos_psql_username',undef),
   $mesos_psql_password = hiera('mesos_psql_password',undef),
-  $mesos_mysql_url      = hiera('mesos_psql_url',undef)
+  $mesos_psql_url      = hiera('mesos_psql_url',undef)
 
 
   case $::osfamily {
