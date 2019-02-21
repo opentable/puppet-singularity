872fd5c87eddbebe3bcaa1e228971b00a095e78f
benpillet <bpillet@opentable.com>
2019-02-21 10:48:02 -0800

    Don't run check_filebeat_exist on each run

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,17 +6,13 @@ class singularity::config inherits singularity {
       notify  => Service['singularity'],
   }
 
-  exec { "check_filebeat_exist":
-    command => "true",
-    path    =>  ["/usr/bin","/usr/sbin", "/bin"],
-    onlyif  => "test -f /etc/filebeat/conf.d/singularity-master.yml"
-  }
-
-  file { "/etc/rsyslog.d/singularity.conf":
-    ensure  => file,
-    content => "if \$programname == 'singularity' then /var/log/singularity.log",
-    require => Exec["check_filebeat_exist"],
-    notify  => Service['filebeat'],
+  if defined(Filebeat::Prospector['singularity-master']) {
+    file { "/etc/rsyslog.d/singularity.conf":
+      ensure  => file,
+      content => "if \$programname == 'singularity' then /var/log/singularity.log",
+      require => Filebeat::Prospector['singularity-master'],
+      notify  => Service['filebeat'],
+    }
   }
 
 }
